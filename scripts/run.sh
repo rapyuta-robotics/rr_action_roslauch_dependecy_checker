9dc1fbaa67709140f06a373d6565c762bc2160d9
Johann Guayacundo Avellaneda <johann.guayacundo@rapyuta-robotics.com>
2022-09-08 17:29:37 +0900

    Corrected exit codes to fail step on failed checks

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,11 +7,14 @@ docker cp $ROOT_PATH/scripts/check.sh $BUILD_CONTAINER_NAME:$SCRIPTS_PATH/check.
 docker cp $ROOT_PATH/scripts/log.sh $BUILD_CONTAINER_NAME:$SCRIPTS_PATH/log.sh
 docker start -a $BUILD_CONTAINER_NAME 2> $out_file
 docker rm $BUILD_CONTAINER_NAME 1> /dev/null
+exit_code=0
 if [ -s $out_file ]
 then
     fatal "Missing dependencies in launch files"
     cat $out_file | grep -v 'wrote test'
+    exit_code=1
 else
     success "All checked files have their dependencies resolved"
 fi
 rm $out_file
+exit $exit_code
