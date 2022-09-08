81c52af805a40c8c6d4d5a967a4f2b63cb9ccc06
Johann Guayacundo Avellaneda <johann.guayacundo@rapyuta-robotics.com>
2022-09-08 17:17:16 +0900

    Corrected destination scripts location

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,8 +3,8 @@ source $ROOT_PATH/scripts/log.sh
 
 out_file="$(cat /proc/sys/kernel/random/uuid).log"
 BUILD_CONTAINER_NAME=$(docker create --workdir $SCRIPTS_PATH --entrypoint "$SCRIPTS_PATH/check.sh" $IMAGE_NAME $ENV_SETUP_PATH $CHECKING_PATH "$CHECKING_EXCLUDES")
-docker cp scripts/check.sh $BUILD_CONTAINER_NAME:$SCRIPTS_PATH/check.sh
-docker cp scripts/log.sh $BUILD_CONTAINER_NAME:$SCRIPTS_PATH/log.sh
+docker cp $ROOT_PATH/scripts/check.sh $BUILD_CONTAINER_NAME:$SCRIPTS_PATH/check.sh
+docker cp $ROOT_PATH/scripts/log.sh $BUILD_CONTAINER_NAME:$SCRIPTS_PATH/log.sh
 docker start -a $BUILD_CONTAINER_NAME 2> $out_file
 docker rm $BUILD_CONTAINER_NAME 1> /dev/null
 if [ -s $out_file ]
