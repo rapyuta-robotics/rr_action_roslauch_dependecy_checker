a456d699c66d05761b1af5687843daf178dc1c51
Johann Guayacundo Avellaneda <johann.guayacundo@rapyuta-robotics.com>
2022-09-09 15:21:54 +0900

    Corrected clean-up routine

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,17 +2,18 @@
 set -e
 source $ROOT_PATH/scripts/log.sh
 
-delete_container() {
-    info "Deleting helper container"
+cleaun_up() {
+    warn "Deleting helper container"
     docker rm $1 1> /dev/null
+    exit 1
 }
 
 out_file="$(cat /proc/sys/kernel/random/uuid).log"
 container_name=$(docker create --workdir $SCRIPTS_PATH --entrypoint "$SCRIPTS_PATH/check.sh" $IMAGE_NAME $ENV_SETUP_PATH $CHECKING_PATH "$CHECKING_EXCLUDES")
-docker cp $ROOT_PATH/scripts/check.sh $container_name:$SCRIPTS_PATH/check.sh || delete_container $container_name
-docker cp $ROOT_PATH/scripts/log.sh $container_name:$SCRIPTS_PATH/log.sh || delete_container $container_name
+docker cp $ROOT_PATH/scripts/check.sh $container_name:$SCRIPTS_PATH/check.sh || cleaun_up $container_name
+docker cp $ROOT_PATH/scripts/log.sh $container_name:$SCRIPTS_PATH/log.sh || cleaun_up $container_name
 docker start -a $container_name 2> $out_file
-delete_container $container_name
+docker rm $container_name 1> /dev/null
 exit_code=0
 if [ -s $out_file ]
 then
